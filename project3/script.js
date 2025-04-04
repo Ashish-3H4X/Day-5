120607df732e088b6d4153ff0de7b322aba74f98
Mr Rahul <ashish227087@gmail.com>
2025-04-04 21:04:04 +0530

    JavaScript function

---
+++ project3/script.js
@@ -0,0 +1,11 @@
+function  multiplication( p1,p2){
+    return p1*p2;  
+}
+
+ document.getElementById('demo1').innerHTML= `
+ function  multiplication( p1,p2){<br>
+    return p1*p2;  <br>
+}<br>
+Output :<br>
+ ${multiplication(2,3)}
+ `;
